080c671079aa931f976b8341c06493bfba1fdf9f
Daniel H <DanHenebery@users.noreply.github.com>
2017-11-19 23:19:01 -0500

    GMM Clustering and GGVizGuide

--- GMM-Tutorial/Code/GMM Clustering Excample.R
+++ GMM-Tutorial/Code/GMM Clustering Excample.R
@@ -5,24 +5,39 @@ gc()
 #Set seed
 set.seed(3)
 
-#Set number of centroids
-g <- 3
 
 #Load packages
 require(mclust)
 require(ggplot2)
+require(BreweryDB)
+require(dplyr)
+
+#Load Example data
+data(iris)
+
+#Set number of centroids
+g <- 3
 
 #Load iris data, create example data
 data(iris)
+
+example2 <- data.frame(x = c(rnorm(300, 0, 10), rnorm(100, 5, 2), rnorm(100, -5, 2)), 
+                 y = c(rnorm(300, 0, 10), rnorm(100, 0, 2), rnorm(100, 0, 2)),
+                 class = c(rep("A",300), rep("B", 100), rep("C", 100)))
+
+#Beer Data (30 is American IPA, 25 is American Pale, 31 is Imperial IPA)
+beer <- list()
+beer$AmericanIPA <- getBeer(n = 400, params = list(styleId = "30"), key = "683b056a8ef0a784770dfeb66c6460ce")
+beer$AmericanPale <- getBeer(n = 400, params = list(styleId = "25"), key = "683b056a8ef0a784770dfeb66c6460ce")
+beer$ImperialIPA <- getBeer(n = 400, params = list(styleId = "31"), key = "683b056a8ef0a784770dfeb66c6460ce")
+beer <- bind_rows(beer)
 
 p0 <- ggplot(iris, aes(x = Petal.Length, y = Petal.Width, color = Species)) + geom_point() + ggtitle("The Iris Dataset")
 png(filename = "iris.png")
 p0
 dev.off()
 
-df <- data.frame(x = c(rnorm(1000, 0, 10), rnorm(500, 5, 2), rnorm(500, -5, 2)), 
-                 y = c(rnorm(1000, 0, 10), rnorm(500, 0, 2), rnorm(500, 0, 2)),
-                 class = c(rep("A",1000), rep("B", 500), rep("C", 500)))
+
 
 p1 <- ggplot(df, aes(x = x, y = y, color = class)) + geom_point() + ggtitle("The Notional Dataset")
 png(filename = "df.png")
@@ -68,4 +83,35 @@ p5 <- ggplot(df, aes(x = x, y = y, color = gmm)) + geom_point()+ ggtitle("GMM on
 png(filename = "df_gmm.png")
 p5
 dev.off()
+
+#Gaussian mixed model (GMM) on beer df
+beer_sub <- beer[!is.na(beer$ibu) & !is.na(beer$abv) & beer$ibu < 180 & beer$abv < 13,]
+beer_mclust <- Mclust(beer_sub[,6:7], G = 3)
+beer_sub$gmm<- LETTERS[as.factor(beer_mclust$classification)]
+table(actual = beer_sub$style_name, cluster = beer_sub$gmm)
+p5 <- ggplot(beer_sub, aes(x = abv, y = ibu, color = gmm)) + geom_point()+ ggtitle("Beer GMM")
+png(filename = "beer_gmm.png")
+p5
+dev.off()
+
+#K-means on beer df
+beer_sub <- beer[!is.na(beer$ibu) & !is.na(beer$abv) & beer$ibu < 180 & beer$abv < 13,]
+
+beer_mclust <- Mclust(beer_sub[,6:7], G = g)
+beer_sub$gmm<- LETTERS[as.factor(beer_mclust$classification)]
+table(actual = beer_sub$style_name, cluster = beer_sub$gmm)
+
+beer_kmeans <- kmeans(beer_sub[,6:7], centers = g)
+beer_sub$kmeans <- as.factor(LETTERS[beer_kmeans$cluster])
+table(actual = beer_sub$style_name, cluster = beer_sub$kmeans)
+
+
+
+table(actual = beer_sub$style_name, cluster = beer_sub$gmm)
+p5 <- ggplot(beer_sub, aes(x = abv, y = ibu, color = gmm)) + geom_point()+ ggtitle("Beer GMM")
+png(filename = "beer_gmm.png")
+p5
+dev.off()
+
+
 

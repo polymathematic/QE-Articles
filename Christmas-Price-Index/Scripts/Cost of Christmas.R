3d355aa1283fc06eeb1f9a0b965bb313d47d46a0
Daniel H <daniel.henebery@gmail.com>
2018-05-31 09:26:55 -0400

    Update Christmas script to use new theme

--- Christmas-Price-Index/Scripts/Cost of Christmas.R
+++ Christmas-Price-Index/Scripts/Cost of Christmas.R
@@ -4,19 +4,22 @@ gc()
 
 #Load packages
 require(ggplot2)
-require(ggthemes)
 require(extrafont)
 require(grid)
 require(quantmod)
 require(scales)
 require(plyr)
-require(reshape)
+require(reshape2)
+require(PlotCC)
 
-#Pull in Data
+# Pull in Data
 xmas <- read.csv("../Data/The Cost of Christmas.csv")
 swans <- read.csv("../Data/SwanPopulation.csv")
 
-#Reshape and Format
+# Load fonts
+LoadFontsCC()
+
+# Reshape and Format
 xmas$YEAR <- as.Date(paste(xmas$YEAR,"-01-01", sep = ""))
 xmas <- melt(xmas, id = "YEAR")
 xmas$value = xmas$value - 1 
@@ -25,99 +28,45 @@ xmas$variable = revalue(xmas$variable, c("CPI" = "Consumer Price Index",
                                          "CPIXMAS_SCALED" = "Christmas Price Index",
                                          "CPIXMAS_CORE_SCALED" = "Christmas Price Index (No Swans)"))
 
-# Define Colors
-QE_colors <- c("#03436F", # Dark Blue
-               "#006AB2", # Mid Blue
-               "#0097FF", # Light Blue
-               "#B26A00", # Dark Orange
-               "#FF9700") # Light Orange
-
-QE_blue_gradient <-c("#0097FF", # lightest blue
-                     "#03436F") # darkest blue
-
-QE_orange_gradient <-c("#FF9700", # lightest orange
-                       "#4e2e00") # darkest orange
-
-#Load OpenSans
-font_import(pattern = "Open")
-y
-loadfonts(device = "win")
-
-#Set Parameters
-axis.title.size <- 14 # Default 14
-axis.text.size <- 11 # Default 11
-legend.title.size <- 14 # Default 14
-legend.text.size <- 11 # Default 11
-plot.title.size <- 20 # Default 20
-width = 720 #720 is width of post column
-height = 500
-
-theme_QE <- theme(text = element_text(family = "Open Sans", color = "#333333"),
-                  
-                  axis.line = element_line(color = "#333333"),
-                  axis.title = element_text(size = axis.title.size),
-                  axis.title.x = element_text(color = "#333333", vjust = -.1),
-                  axis.title.y = element_text(vjust = .8),
-                  axis.text = element_text(size = axis.text.size, color = "#333333"),
-                  axis.ticks = element_line(color = "#333333", size = .3),
-                  axis.ticks.length = unit(4,"points"),
-                  axis.ticks.margin = unit(4,"points"),
-                  
-                  legend.title = element_blank(),
-                  legend.background = element_rect(fill = NA),
-                  legend.text = element_text(size = legend.text.size),
-                  legend.justification = c(0,0),
-                  legend.margin = unit(0,"points"),
-                  legend.position = "bottom",
-                  legend.direction = "horizontal",
-                  legend.box.just = "right",
-                  legend.key = element_rect(fill = NA),
-                  legend.key.size = unit(12,"pt"),
-                  
-                  panel.border = element_blank(),
-                  panel.background = element_rect(fill = NA), #change to NA
-                  panel.grid.major = element_blank(),
-                  panel.grid.minor = element_blank(),
-                  panel.margin = unit(20,"points"),
-                  
-                  plot.margin = unit(c(20,20,20,20), "points"), #Change the 3rd number to 5 if there's no label for x
-                  plot.title = element_text(size = plot.title.size, face = "bold", hjust = 0, vjust = 1.2))
-
-# Plot Indices over time
 xmas$variable <- ordered(xmas$variable, levels = c("Swan Price",
                                                    "Consumer Price Index",
                                                    "Christmas Price Index",
                                                    "Christmas Price Index (No Swans)"))
 
-svg(filename = "../Visuals/PriceIndices.svg",
-    width = (width*3)/(72*4),
-    height = (height*3)/(72*4),
-    family = "Open Sans",
-    antialias = "none")
-
+# Set date breaks
 break.vec<-c(as.Date("1980-1-1"),
              seq(from=as.Date("1985-1-1"), to=as.Date("2010-1-1"), by="5 year"),
              as.Date("2015-1-1"))
 
-plot <- ggplot(xmas, aes(y = value, x = YEAR, color = variable))
-plot <- plot + ggtitle("Comparison of Price Indices")
-plot <- plot + ylab("Percent Change Since 1984")
-plot <- plot + geom_line(size = 1)
-plot <- plot + theme_QE
-plot <- plot + scale_color_manual(values = c(QE_colors[5], QE_colors[3], QE_colors[2], QE_colors[1]))
-plot <- plot + scale_y_continuous(limits = c(-1,3),
-                                  expand = c(0,0),
-                                  labels = percent)
-plot <- plot + theme(axis.title.x = element_blank(),
-                     axis.text.x = element_text(hjust = 1.1, vjust = 1.1, angle = 45))
-plot <- plot + scale_x_date(breaks = break.vec,
-                            labels = date_format("%Y"),
-                            limits = c(as.Date("1984-1-1"), as.Date("2015-1-1")),
-                            expand = c(0,0))
-plot <- plot + guides(col = guide_legend(nrow = 2))
-plot
+# Plot Indices over time
+plot <- ggplot(xmas, aes(y = value, x = YEAR, color = variable)) +
+  labs(title = toupper("Price Index Comparison"),
+         subtitle = toupper("Percent change in price levels since 1984"),
+         caption = 'Source: PNC Bank, Federal Reserve Economic Data (FRED)') +
+  ylab("Percent Change Since 1984") +
+  xlab("") +
+  geom_line(size = 1) +
+  scale_y_continuous(limits = c(-1,3),
+                     expand = c(0,0),
+                     labels = percent) +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_text(hjust = 1.1,
+                                   vjust = 1.1,
+                                   angle = 45)) +
+  scale_x_date(breaks = break.vec,
+               labels = date_format("%Y"),
+               limits = c(as.Date("1984-1-1"), as.Date("2015-1-1")),
+               expand = c(0,0)) +
+  guides(col = guide_legend(nrow = 2)) +
+  GenerateThemeCC(hlines = TRUE) +
+  scale_color_manual(values = c(palette_base_cc[[2]], palette_base_cc[[3]], palette_base_cc[[4]], palette_base_cc[[5]]))
+plot 
 
-dev.off()
+exportPlot(plot, path = "../Graphics", name = "PriceIndices_1984")
+
+
+
+
 
 #Plot swan quantity/price
 # swans.lm <- lm(P ~ SWANPOP, data = swans)
